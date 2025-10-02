6c823cd832e3d323c3b6187d7f0195f7301063c4
FreeLearner-dot <freemiumu79@gmail.com>
2025-10-02 16:53:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -141,4 +141,5 @@ def save_score():
 if __name__ == '__main__':
     init_db()
     app.debug = True
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+    app.run(debug=True, host='0.0.0.0', port=10000)
